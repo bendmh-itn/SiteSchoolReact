2723149d43f960485b1eec842e07e57621000431
unknown <demab@site.asty-moulin.be>
2021-02-02 11:59:29 +0100

    ajout des storyBoards

--- src/Pages/pageEleves.jsx
+++ src/Pages/pageEleves.jsx
@@ -156,180 +156,215 @@ const dataSciences = [
   {
     "Nom": "Malburny",
     "Prénom": "Aaron",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Aaron Malburny - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Aaron Malburny - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/19OQDWzeR-5Xx6wjkwunb1xbCX6OAE_ZE/view?usp=sharing"
   },
   {
     "Nom": "Robat",
     "Prénom": "Alexandre",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Alexandre Robat - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Alexandre Robat - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1t0eQz-7PfByj8vZIzSaJ8aSH7xTaGmEc/view?usp=sharing"
   },
   {
     "Nom": "Byloos",
     "Prénom": "Gabriel",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Gabriel Byloos - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Gabriel Byloos - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1pECqVQIHY-Sclwz5yTHD5vugb343C2lt/view?usp=sharing"
   },
   {
     "Nom": "Vanesse",
     "Prénom": "Hugo",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Hugo Vanesse - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Hugo Vanesse - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1ZTC2dQn4aIjtwEfDYNdl3p2bRqcVJgjU/view?usp=sharing"
   },
   {
     "Nom": "Muno",
     "Prénom": "Julien",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Julien Muno - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Julien Muno - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Vanderlinden",
     "Prénom": "Kimberly",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Kimberly Vanderlinden - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Kimberly Vanderlinden - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Lambeau",
     "Prénom": "Lyze",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Lyze Lambeau - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Lyze Lambeau - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Van Nuffel",
     "Prénom": "Nicolas",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Nicolas Van Nuffel - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Nicolas Van Nuffel - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Hardij",
     "Prénom": "Noah",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Noah Hardij - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Noah Hardij - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Alhelali",
     "Prénom": "Razan",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Razan Alhelali - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Razan Alhelali - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1doFkaoQFr_xaaHX1vSPz7Ya0OS_I5DBa/view?usp=sharing"
   },
   {
     "Nom": "Hustin",
     "Prénom": "Romain",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Romain Hustin - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Romain Hustin - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Hody",
     "Prénom": "Véréna",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Véréna Hody - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Véréna Hody - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Davister",
     "Prénom": "Zora",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Zora Davister - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Zora Davister - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1_b7HuUbA5HgBZaU7riWBLQ7JqCwi6a0K/view?usp=sharing"
   }
  ]
 
  const dataEco2 = [
   {
-    "Nom": "Acosta Perez",
-    "Prénom": "Harold",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Harold Acosta Perez - index.html"
-  },
-  {
-    "Nom": "Aibekova",
-    "Prénom": "Tinatin",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Tinatin Aibekova - index.html"
-  },
-  {
-    "Nom": "Ajdini",
-    "Prénom": "Lorik",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Lorik Ajdini - index.html"
-  },
-  {
-    "Nom": "Aktas",
-    "Prénom": "Ibrahim",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Ibrahim Aktas - index.html"
+    "Nom": "Restieau",
+    "Prénom": "Alicia",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Alicia Restieau - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Boulanger",
     "Prénom": "Amandine",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Amandine Boulanger - index.html"
-  },
-  {
-    "Nom": "Cheval",
-    "Prénom": "Chloé",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Chloé Cheval - index.html"
-  },
-  {
-    "Nom": "Dedobbeleer",
-    "Prénom": "Sarah",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Sarah Dedobbeleer - index.html"
-  },
-  {
-    "Nom": "Dernivoy",
-    "Prénom": "Chloé",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Chloé Dernivoy - index.html"
-  },
-  {
-    "Nom": "Gil Y Rodriguez",
-    "Prénom": "Noah",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Noah Gil Y Rodriguez - index.html"
-  },
-  {
-    "Nom": "Golard",
-    "Prénom": "Wyatt",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Wyatt Golard - index.html"
-  },
-  {
-    "Nom": "Huart",
-    "Prénom": "Chelsea",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Chelsea Huart - index.html"
-  },
-  {
-    "Nom": "Izere",
-    "Prénom": "Esther",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Esther Izere - index.html"
-  },
-  {
-    "Nom": "Leonard",
-    "Prénom": "Nathan",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Nathan Leonard - index.html"
-  },
-  {
-    "Nom": "Morre",
-    "Prénom": "Hugo",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Hugo Morre - index.html"
-  },
-  {
-    "Nom": "Murtezi",
-    "Prénom": "Artan",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Artan Murtezi - index.html"
-  },
-  {
-    "Nom": "Renier",
-    "Prénom": "Louka",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Louka Renier - index.html"
-  },
-  {
-    "Nom": "Restieau",
-    "Prénom": "Alicia",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Alicia Restieau - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Amandine Boulanger - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1EZOVgm3qGpMeEgC7IwFsQgqzOURU3JDw/view?usp=sharing"
   },
   {
     "Nom": "Sabani",
     "Prénom": "Amir",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Amir Sabani - index.html"
-  },
-  {
-    "Nom": "Sarikaya",
-    "Prénom": "Asim",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Asim Sarikaya - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Amir Sabani - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Thuin",
     "Prénom": "Amélie",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Amélie Thuin - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Amélie Thuin - index.html",
+    "StoryBoard 1": ""
   },
   {
     "Nom": "Vanderuse",
     "Prénom": "Angélique",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Angélique Vanderuse - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Angélique Vanderuse - index.html",
+    "StoryBoard 1": ""
+  },
+  {
+    "Nom": "Murtezi",
+    "Prénom": "Artan",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Artan Murtezi - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/11QcgtFPjOPsALex7KJ0IwkPHghKlzc_u/view?usp=sharing"
+  },
+  {
+    "Nom": "Sarikaya",
+    "Prénom": "Asim",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Asim Sarikaya - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1z7pMSRN6pZ77k2RqVTnyanFGkytgiJWS/view?usp=sharing"
+  },
+  {
+    "Nom": "Huart",
+    "Prénom": "Chelsea",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Chelsea Huart - index.html",
+    "StoryBoard 1": ""
+  },
+  {
+    "Nom": "Cheval",
+    "Prénom": "Chloé",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Chloé Cheval - index.html",
+    "StoryBoard 1": ""
+  },
+  {
+    "Nom": "Dernivoy",
+    "Prénom": "Chloé",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Chloé Dernivoy - index.html",
+    "StoryBoard 1": ""
+  },
+  {
+    "Nom": "Izere",
+    "Prénom": "Esther",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Esther Izere - index.html",
+    "StoryBoard 1": ""
+  },
+  {
+    "Nom": "Acosta Perez",
+    "Prénom": "Harold",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Harold Acosta Perez - index.html",
+    "StoryBoard 1": ""
+  },
+  {
+    "Nom": "Morre",
+    "Prénom": "Hugo",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Hugo Morre - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1DLX71AT__OavPwrZZNyHDQZ-qANMphv_/view?usp=sharing"
+  },
+  {
+    "Nom": "Aktas",
+    "Prénom": "Ibrahim",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Ibrahim Aktas - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1doFkaoQFr_xaaHX1vSPz7Ya0OS_I5DBa/view?usp=sharing"
+  },
+  {
+    "Nom": "Ajdini",
+    "Prénom": "Lorik",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Lorik Ajdini - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1UkJfaMNkEbYwnp0gzHahBOSeWTdzkfXf/view?usp=sharing"
+  },
+  {
+    "Nom": "Renier",
+    "Prénom": "Louka",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Louka Renier - index.html",
+    "StoryBoard 1": ""
+  },
+  {
+    "Nom": "Leonard",
+    "Prénom": "Nathan",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Nathan Leonard - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1s9liaX0HK_dmBFmro1vgtloej-re5l7X/view?usp=sharing"
+  },
+  {
+    "Nom": "Gil Y Rodriguez",
+    "Prénom": "Noah",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Noah Gil Y Rodriguez - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1VZfre1N0CNnOYt_Uajl4w_yG8SN6OUf6/view?usp=sharing"
+  },
+  {
+    "Nom": "Dedobbeleer",
+    "Prénom": "Sarah",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Sarah Dedobbeleer - index.html",
+    "StoryBoard 1": ""
+  },
+  {
+    "Nom": "Aibekova",
+    "Prénom": "Tinatin",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Tinatin Aibekova - index.html",
+    "StoryBoard 1": ""
+  },
+  {
+    "Nom": "Golard",
+    "Prénom": "Wyatt",
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Wyatt Golard - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/108TOvH8f8ECJFW8G6ESOJ5c-iAAFQamd/view?usp=sharing"
   },
   {
     "Nom": "Yasar",
     "Prénom": "Yasin",
-    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Yasin Yasar - index.html"
+    "Site web": "https://bendmh-itn.github.io/siteschool/site2eme/Yasin Yasar - index.html",
+    "StoryBoard 1": "https://drive.google.com/file/d/1ug81emQy9Y1g9MdZJ5P5kNp6qDoXQXN4/view?usp=sharing"
   }
  ]
 
