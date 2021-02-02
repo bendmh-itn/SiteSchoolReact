eeb6dadfc8bfcaef606d570d649a8ea8f17efd78
unknown <demab@site.asty-moulin.be>
2021-02-02 13:58:41 +0100

    gestion des images

--- src/Pages/myStoryBoard.jsx
+++ src/Pages/myStoryBoard.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 const MyStoryBoard = () => {
     return ( 
         <>
-            <img src="https://sbt.blob.core.windows.net/classroom-public/529ee40c/malheur-chez-le-dentiste.png" />
-            <img src="https://sbt.blob.core.windows.net/classroom-public/529ee40c/malheur-chez-le-dentiste---page-2.png" />
+            <div className="containerFlex">
+                <div className="elementFlexible">
+                    <img src="https://sbt.blob.core.windows.net/classroom-public/529ee40c/malheur-chez-le-dentiste.png" />
+                </div>
+                <div className="elementFlexible">
+                    <img src="https://sbt.blob.core.windows.net/classroom-public/529ee40c/malheur-chez-le-dentiste---page-2.png" />
+                </div>
+            </div>
         </>
      );
 }
